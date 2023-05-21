e09b4b68374edfcc79e39173b524b0e50caaac0e
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-05-21 13:35:14 +0530

    Get method completed

--- index.js
+++ index.js
@@ -5,4 +5,34 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+const products = [
+  {
+    id: '1',
+    name: 'orange',
+    price: 20,
+  },
+  {
+    id: '2',
+    name: 'apple',
+    price: 30,
+  },
+]
+
+//Show list of all products
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+//Show specific products
+app.get('/api/products/:id', (req, res) => {
+  const { id } = req.params
+  const product = products.find((b) => b.id === id)
+
+  if (!product) {
+    return res.status(404).json({
+      error: 'No product found with this ID',
+    })
+  }
+  return res.json(product)
+})
 app.listen(3000, () => console.log('Server is running on port 3000'))
