053e203e5494fb29c419db23060e00cd64095229
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-05-21 18:49:12 +0530

    Post method completed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const { v4: uuidv4 } = require('uuid')
+const Joi = require('joi')
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -35,4 +37,29 @@ app.get('/api/products/:id', (req, res) => {
   }
   return res.json(product)
 })
+
+//Insert a product
+app.use(express.json())
+app.post('/api/products', (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(20).required(),
+    price: Joi.number().required(),
+  })
+
+  const { error } = schema.validate(req.body)
+
+  if (error) {
+    return res.status(400).json({
+      message: error.details[0].message,
+    })
+  }
+  const product = {
+    id: uuidv4(),
+    name: req.body.name,
+    price: req.body.price,
+  }
+  products.push(product)
+  return res.json(products)
+})
+
 app.listen(3000, () => console.log('Server is running on port 3000'))
